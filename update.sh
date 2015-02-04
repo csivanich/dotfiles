535e98df788ac63d649aa91206d2df9df3889815
Chris Sivanich <csivanich@gmail.com>
2015-02-04 15:01:09 -0600

    Updated update script to work better with non master branches

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 git fetch -vp && \
-git rebase origin/master && \
+git rebase origin/"$(git rev-parse --abbrev-ref HEAD)" && \
 git submodule update --init --recursive && \
 ./install.sh
