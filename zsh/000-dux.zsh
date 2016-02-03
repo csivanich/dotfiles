65c751da50b1141b76683123a8c69cc21ec7c49a
Chris Sivanich <csivanich@gmail.com>
2016-02-03 09:54:40 -0600

    Updated zsh to exit after dux exits

--- zsh/000-dux.zsh
+++ zsh/000-dux.zsh
@@ -1,3 +1,10 @@
 if [[ ! $TERM =~ screen  ]]; then
-    ~/bin/dux || echo "Failed to run Dux"
+    ~/bin/dux || echo "Failed to run Dux (returned with $?)"
+    echo -n "Exiting in 3 "
+    sleep 1
+    echo -n "2 "
+    sleep 1
+    echo -n "1 "
+    sleep 1
+    exit
 fi
