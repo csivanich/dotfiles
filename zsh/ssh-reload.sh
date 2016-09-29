d9cf9dce63c2e55e3e61249bfae3811829feec54
Chris Sivanich <csivanich@gmail.com>
2016-09-28 22:56:56 -0500

    Updated command silencing in ssh-reload.sh

--- zsh/ssh-reload.sh
+++ zsh/ssh-reload.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 start_ssh_agent() {
-    which ssh-agent >/dev/null 2>&1 || return
+    which ssh-agent &>/dev/null || return
     killall ssh-agent
     rm ~/.zsh.d/ssh-output
     ssh-agent | grep -v "echo" > ~/.zsh.d/ssh-output
 }
 
-pgrep ssh-agent >/dev/null 2>&1 || start_ssh_agent
+pgrep ssh-agent &>/dev/null || start_ssh_agent
