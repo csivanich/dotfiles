d9b7af56ef6cb065c5fa98807b6d99084b10bcc9
Chris Sivanich <csivanich@gmail.com>
2016-11-03 23:01:10 -0500

    Added new default prompt color file

---
+++ zsh/000-prompt-colors.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Possible colors:
+# black
+# blue
+# green
+# magenta
+# red
+# white
+# yellow
+
+_p_load_colors(){
+    C_BG="black"
+    C_FG="white"
+    C_HOSTNAME="yellow"
+    C_JOBS="yellow"
+    C_RUBY="red"
+    C_BRANCH="blue"
+    C_BRANCH_TEXT="white"
+    C_ADDITIONS="green"
+    C_CHANGES="yellow"
+    C_DELETIONS="red"
+    C_BATTERY="blue"
+}
+
+_p_load_colors
