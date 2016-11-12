e3f18d580e7642e03aef03709c80209d0b320f10
Chris Sivanich <csivanich@gmail.com>
2016-11-12 15:34:51 -0600

    Updated zsh exports
    - Removed ruby gem PATHs, rvm does that since `32cd279`
    - Updated $HOME/bin as first priority in $PATH

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -8,15 +8,8 @@ export EDITOR='vim'
 # Default terminal is xterm
 export TERMINAL=xterm
 
-# Stock Arch Linux
-export ARCHFLAGS="-arch x86_64"
-
 # If Perl is found, add to PATH
 test -d "/usr/bin/vendor_perl" && PATH="$PATH:/usr/bin/vendor_perl"
 
 # If we have a local bin, add it
-test -d "$HOME/bin" && PATH="$PATH:$HOME/bin"
-
-# If we have a local ruby bin, add it
-test -d "$HOME/.gem/ruby/1.9.1/bin/" && PATH="$PATH:$HOME/.gem/ruby/1.9.1/bin/"
-test -d "$HOME/.gem/ruby/2.2.0/bin/" && PATH="$PATH:$HOME/.gem/ruby/2.2.0/bin/"
+test -d "$HOME/bin" && PATH="$HOME/bin:$PATH"
